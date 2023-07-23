2f8997ce04f0823c52e96f0e388a7a20e264d961
codeSenseiRoman <88555786+codeSenseiRoman@users.noreply.github.com>
2023-07-23 00:31:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,23 @@ const possibleAnswers = [
     'For sure!',
     'Why Not!',
     'No way José!',
-    "I wish, BUT NO!",
+    "I'd love to, BUT I can't!",
     'No!',
     'Nope!',
     'Oh heck no!',
-    'NEVER!'
+    'NEVER!',
 ]
 
 const magic8Ball = document.querySelector('#ballBackground');
 const textArea = document.querySelector('#textArea');
-
 magic8Ball.addEventListener('click', function(event) {
     const randomNumber = Math.floor(Math.random() * 10);
     const randomPick = possibleAnswers[randomNumber];
-    
     if(randomNumber > 0 && randomNumber <= 4 ||
         randomNumber >= 8) {
             textArea.style.fontSize = '2em';
         } else if(randomNumber === 5) {
-            textArea.style.fontSize = '1.8em';
+            textArea.style.fontSize = '1em';
         }
         else {
             textArea.style.fontSize = '4em';
